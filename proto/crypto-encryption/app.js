5a415fa9b1bfa7c5bb34dfbb7e699a4f9ff8a2bd
Sushant Sahu <sushant87@gmail.com>
2018-11-05 01:40:51 +0530

    encryption works

--- proto/crypto-encryption/app.js
+++ proto/crypto-encryption/app.js
@@ -9,47 +9,29 @@ const publickey = fs.readFileSync(path.join(__dirname, './public.pem'), 'utf8');
 // console.log(publickey);
 
 const pass_phrase = "hognose thruster blissful agger carmine minium scythe soapsuds cynosure gestate roundlet wood";
-const encrypt_text = {
+let encrypt_text = {
     name: "sushant",
     password: "test@123"
 }
 
-const encryptStringWithRsaPublicKey = function(toEncrypt, relativeOrAbsolutePathToPublicKey) {
-    const absolutePath = path.resolve(relativeOrAbsolutePathToPublicKey);
-    const publicKey = fs.readFileSync(absolutePath, "utf8");
-    const buffer = new Buffer(toEncrypt);
-    const encrypted = crypto.publicEncrypt(publicKey, buffer);
-    return encrypted.toString("base64");
-};
+encrypt_text = JSON.stringify(encrypt_text);
 
-const decryptStringWithRsaPrivateKey = function(toDecrypt, relativeOrAbsolutePathtoPrivateKey) {
-    const absolutePath = path.resolve(relativeOrAbsolutePathtoPrivateKey);
-    const privateKey = fs.readFileSync(absolutePath, "utf8");
-    const buffer = new Buffer(toDecrypt, "base64");
-    const decrypted = crypto.privateDecrypt(privateKey, buffer);
-    return decrypted.toString("utf8");
-};
+let key_privatekey = {
+    key: privatekey,
+    passphrase: pass_phrase,
+    padding: crypto.constants.RSA_PKCS1_PADDING
+}
 
-const encryptedContent = encryptStringWithRsaPublicKey(encrypt_text.toString(), './public.pem');
-console.log(encryptedContent);
+let buffer = new Buffer(encrypt_text);
+let encryptedContent = crypto.privateEncrypt(privatekey.toString(), buffer);
+console.log(encryptedContent.toString("base64"));
 
-const decryptedContent = decryptStringWithRsaPrivateKey(encryptedContent, './private.pem');
+let key_publickey = {
+    key: publickey,
+    passphrase: pass_phrase,
+    padding: crypto.constants.RSA_PKCS1_PADDING
+}
+
+let decryptedContent = crypto.publicDecrypt(publickey.toString(), encryptedContent);
+decryptedContent = decryptedContent.toString("utf8");
 console.log(decryptedContent);
-
-
-// const key_privatekey = {
-//     key: privatekey,
-//     passphrase: pass_phrase,
-//     padding: crypto.constants.RSA_PKCS1_PADDING
-// }
-// const buffer = new Buffer(encrypt_text.toString());
-// const encryptedContent = crypto.privateEncrypt(key_privatekey.toString(), buffer);
-// console.log(Buffer.from(encrypt_text).toString('base64'));
-
-// const key_publickey = {
-//     key: publickey,
-//     passphrase: pass_phrase,
-//     padding: crypto.constants.RSA_PKCS1_PADDING
-// }
-// const decryptedContent = crypto.publicDecrypt(key_publickey.toString(), encryptedContent);
-// console.log(decryptedContent);
