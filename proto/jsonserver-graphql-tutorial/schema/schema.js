fea5e3eedb070830fb046bb65099159ffd6acd81
Sushant Sahu <sushant87@gmail.com>
2018-11-21 18:56:37 +0530

    graph ql example using a dynamic database

---
+++ proto/jsonserver-graphql-tutorial/schema/schema.js
@@ -0,0 +1,32 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const axios = require('axios');
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/users/${args.id}`)
+              .then(resp => resp.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
