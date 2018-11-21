93c6fe8ef49a1d5b8a0c2bccf1aa9d638af45ab0
Sushant Sahu <sushant87@gmail.com>
2018-11-21 20:00:03 +0530

    added additional models

--- proto/jsonserver-graphql-tutorial/schema/schema.js
+++ proto/jsonserver-graphql-tutorial/schema/schema.js
@@ -2,12 +2,26 @@ const graphql = require("graphql");
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 const axios = require('axios');
 
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  }
+});
+
 const UserType = new GraphQLObjectType({
   name: "User",
   fields: {
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    company: {type: CompanyType,
+    resolve(parentValue, args) {
+      return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+            .then(resp => resp.data);
+    }}
   }
 });
 
@@ -23,7 +37,18 @@ const RootQuery = new GraphQLObjectType({
         return axios.get(`http://localhost:3000/users/${args.id}`)
               .then(resp => resp.data);
       }
+    },
+    company: {
+      type: CompanyType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`)
+              .then(resp => resp.data);
+      }
     }
+
   }
 });
 
