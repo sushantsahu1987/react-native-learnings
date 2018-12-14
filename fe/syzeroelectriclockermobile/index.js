bbedab931f382fe854887d06826409b2e59e4917
Sushant Sahu <sushant87@gmail.com>
2018-12-14 16:04:02 +0530

    Added sagas, additional screens like edit & delete

--- fe/syzeroelectriclockermobile/index.js
+++ fe/syzeroelectriclockermobile/index.js
@@ -1,7 +1,27 @@
 /** @format */
-
 import {AppRegistry} from 'react-native';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
+import createSagaMiddleware from "redux-saga";
+
+import loginReducer from "./store/reducers/login_reducer";
+import { loginSaga } from "./store/sagas";
+
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const rootReducer = combineReducers({
+    login: loginReducer
+  });
+  
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));  
+sagaMiddleware.run(loginSaga);
+
+const ElektrikApp = () => (
+    <Provider store={store}>
+      <App />
+    </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => ElektrikApp);
